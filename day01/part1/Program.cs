56e54c02d437b4e821416b4f3a02ecc3f9f8ca96
Alex Mickelson <alexmickelson96@gmail.com>
2023-11-30 22:37:29 -0700

    finished part 1, regretting not doing tdd

---
+++ day01/part1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+// var part1Input = @"1abc2
+// pqr3stu8vwx
+// a1b2c3d4e5f
+// treb7uchet";
+
+var part1Input = File.ReadAllText("part1Input.txt");
+
+var digitsAsStrings = part1Input
+  .Split(Environment.NewLine)
+  .Select(l => 
+    l.First(c => char.IsDigit(c)) + "" 
+    + l.Last(c => char.IsDigit(c))
+  );
+
+var sum = digitsAsStrings.Select(s => int.Parse(s)).Sum();
+
+Console.WriteLine(JsonSerializer.Serialize(digitsAsStrings));
+Console.WriteLine(sum);
---
+++ day01/part2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.Json;
+
+// var input = File.ReadAllText("testInput.txt");
+var input = File.ReadAllText("realInput.txt");
+
+var digitsAsStrings = input
+  .Split(Environment.NewLine)
+  .Select(l =>
+    firstNumber(l) + 
+    lastNumber(string.Join("", l.Reverse()))
+  );
+
+var sum = digitsAsStrings.Select(s => int.Parse(s)).Sum();
+
+Console.WriteLine(JsonSerializer.Serialize(digitsAsStrings));
+Console.WriteLine(sum);
+
+
+
+string firstNumber(string input)
+{
+  var spelledNumbers = new string[]
+  {
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  };
+  if (char.IsDigit(input.First()))
+    return input.First().ToString();
+  
+  foreach(var spelledNumber in spelledNumbers)
+  {
+    if(input.StartsWith(spelledNumber))
+    {
+      return convertToNumber(spelledNumber);
+    }
+  }
+  return firstNumber(input[1..]);
+}
+string lastNumber(string invertedInput)
+{
+  var spelledNumbers = new string[]
+  {
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  };
+  if (char.IsDigit(invertedInput.First()))
+    return invertedInput.First().ToString();
+  
+  foreach(var spelledNumber in spelledNumbers)
+  {
+    var reversedNumber = string.Join("", spelledNumber.Reverse());
+    if(invertedInput.StartsWith(reversedNumber))
+    {
+      return convertToNumber(spelledNumber);
+    }
+  }
+  return lastNumber(invertedInput[1..]);
+}
+
+static string convertToNumber(string spelledNumber)
+{
+  return spelledNumber switch
+  {
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9",
+    _ => throw new Exception("invalid spelled number " + spelledNumber),
+  };
+}
